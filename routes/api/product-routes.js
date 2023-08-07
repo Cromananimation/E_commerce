263f74af8e2ce56281af55a0aed9ed9e495d036d
Cesar A. Roman <130246064+Cromananimation@users.noreply.github.com>
2023-08-07 17:40:24 -0500

    add all the neccesary routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,27 +4,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const driverData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag }, { model: ProductTag}],
+    });
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
+  try {
+const productData = await Product.findByPk(req.params.id, {
+  include: [{ model: Category }, { model: Tag }, { model: ProductTag}],
+})
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
-  /* req.body should look like this...
+router.post('/', async (req, res) => {
+  // req.body should look like this...
     {
       product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      price; 200.00,
+      stock; 3,
+      tagIds; [1, 2, 3, 4]
     }
-  */
+  
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -94,6 +109,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    
+  } catch (err) {
+    res.status(500).json(error);
+    
+  }
 });
 
 module.exports = router;
